ee6908aa586c8f3fa53074a19ebc61d52481cba8
Simon Phillips <poc_is_good@hotmail.com>
2025-05-11 14:45:36 +0100

    feat: add depdencies to install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
-# fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
-
-# nvm
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
-
 sudo apt install zoxide
 sudo apt install stow
+sudo apt install jq
+
+cd ~/dotfiles
+stow .
+
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+
+~/.fzf/install
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+
+git clone git@github.com:Si2k63/AstroNvimV4.git ~/.config/nvim

cea0a8a721ecaf4ecc10d0783374eb4710c8dce3
Simon Phillips <poc_is_good@hotmail.com>
2026-05-02 15:09:42 +0100

    merge branch: 'main' into 'main'

--- layouts/editor.window.sh
+++ layouts/editor.window.sh
@@ -1,14 +1,62 @@
-# Set window root path. Default is `$session_root`.
-# Must be called before `new_window`.
-window_root "$(pwd)"
+# 1. Load your shared logic
+source ~/dotfiles/.aliases
 
-# Create new window. If no argument is given, window name will be based on
-# layout file name.
-new_window "editor"
-run_cmd "nvim"
-split_v 15
-split_h 50
-run_cmd "gst"
-select_pane 2
-run_cmd "nvm_use.sh"
-run_cmd "package_json_script_selector.sh"
+# 2. Validate the project root immediately
+PROJ=$(wproot)
+
+if [ -z "$PROJ" ]; then
+  echo "----------------------------------------------------------"
+  echo "ABORTING: Not a valid WordPress project root."
+  echo "----------------------------------------------------------"
+  exit 1
+fi
+
+# 3. Theme Window (First Window)
+# We calculate the path first to see if we should create the window
+THEME_PATH=$(get_theme_path)
+
+if [[ -n "$THEME_PATH" ]]; then
+  window_root "$THEME_PATH"
+  tmux rename-window "editor"
+  split_v 15
+  run_cmd "gst"
+  select_pane 1
+  run_cmd "nvim"
+fi
+
+# 4. Accommodation Plugin
+if [[ -d "$PROJ/web/wp-content/plugins/accommodation" ]]; then
+  window_root "$PROJ/web/wp-content/plugins/accommodation"
+  new_window "acc"
+  run_cmd "nvim ."
+  split_v 15
+  run_cmd "gst"
+  select_pane 1
+fi
+
+# 5. Supercontrol Plugin
+if [[ -d "$PROJ/web/wp-content/plugins/supercontrol" ]]; then
+  window_root "$PROJ/web/wp-content/plugins/supercontrol"
+  new_window "sc"
+  run_cmd "nvim ."
+  split_v 15
+  run_cmd "gst"
+  select_pane 1
+fi
+
+# 6. Web Window
+if [[ -d "$PROJ/web" ]]; then
+  window_root "$PROJ/web"
+  new_window "web"
+fi
+
+# 7. Logs
+if [[ -d "$PROJ/log" ]]; then
+  window_root "$PROJ/log"
+  new_window "logs"
+fi
+
+new_window "htop"
+run_cmd "htop"
+
+select_window 1
--- scripts/tmux_session_manager.sh
+++ scripts/tmux_session_manager.sh
@@ -1,46 +1,44 @@
 #!/bin/bash
 
 for arg in "$@"; do
-    case "$arg" in
-        --editor) EDITOR_MODE=true ;;
-    esac
+  case "$arg" in
+  --editor) EDITOR_MODE=true ;;
+  esac
 done
 
 main() {
-    local site_directory="/home/$USER/Projects"
-    selected_dir=$(fzf --style full \
+  local site_directory="/var/www/"
+  selected_dir=$(fzf --style full \
     --padding 1,2 \
     --color 'border:#aaaaaa,label:#cccccc' \
     --color 'list-border:#00AACC,list-label:#99cc99' \
     --color 'input-border:#00CCAA,input-label:#ffcccc' \
     --color 'header-border:#6699cc,header-label:#ffffff' < <(
-    find -L "$site_directory" -mindepth 1 -maxdepth 1  &&
-    echo "/home/$USER/dotfiles"
+      find -L "$site_directory" -mindepth 1 -maxdepth 1 &&
+        echo "/home/$USER/dotfiles"
     ))
 
-    if [[ -n $selected_dir && -d $selected_dir ]]; then
-        cd "$selected_dir"
-        initialise_tmux
-    fi
+  if [[ -n $selected_dir && -d $selected_dir ]]; then
+    cd "$selected_dir"
+    initialise_tmux
+  fi
 }
 
 initialise_tmux() {
-    local session_name=$(basename "$PWD")
-    local session_exists=$(tmux has-session -t $session_name 2>/dev/null)
+  local session_name=$(pwd | cut -d'/' -f4)
+  local session_exists=$(tmux has-session -t $session_name 2>/dev/null)
 
-    if [ session_exists != 0 ]; then
-        if [ "$EDITOR_MODE" = "true" ]; then
-            tmux new-session -d -s $session_name "tmuxifier load-window editor ; tmux select-window -t 0"
-        else
-            tmux new-session -d -s $session_name
-        fi
-    fi
+  if [ session_exists != 0 ]; then
+    tmux new-session -d -s $session_name
+  else
+    tmux attach -s $session_name
+  fi
 
-    if [ -n "$TMUX" ]; then
-        tmux switch-client -t $session_name
-    else
-        tmux attach -t $session_name
-    fi
+  if [ -n "$TMUX" ]; then
+    tmux switch-client -t $session_name
+  else
+    tmux attach -t $session_name
+  fi
 }
 
 main
